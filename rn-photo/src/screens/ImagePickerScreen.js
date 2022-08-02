72adf29900920d26de2b1c97d5e0f110c5702f13
Beomjun <alchemist85k@gmail.com>
2022-08-02 16:01:23 -0700

    9.2.4.1

--- rn-photo/src/screens/ImagePickerScreen.js
+++ rn-photo/src/screens/ImagePickerScreen.js
@@ -1,10 +1,28 @@
 import { useNavigation } from '@react-navigation/native';
-import { useLayoutEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useLayoutEffect } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import HeaderRight from '../components/HeaderRight';
+import * as MediaLibrary from 'expo-media-library';
 
 const ImagePickerScreen = () => {
   const navigation = useNavigation();
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  useEffect(() => {
+    (async () => {
+      const { granted } = await requestPermission();
+      if (!granted) {
+        Alert.alert('사진 접근 권한', '사진 접근 권한이 필요합니다.', [
+          {
+            text: '확인',
+            onPress: () => {
+              navigation.canGoBack() && navigation.goBack();
+            },
+          },
+        ]);
+      }
+    })();
+  }, [navigation, requestPermission]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
