97d89fd70c74b2c24cc0205e3649b40984ea60a3
Beomjun <alchemist85k@gmail.com>
2022-08-02 00:50:34 -0700

    5.1.3.2 remove code

--- rn-todo/src/screens/ListScreen.js
+++ rn-todo/src/screens/ListScreen.js
@@ -1,17 +1,9 @@
-/* eslint-disable react/prop-types */
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const ListScreen = ({ navigation, route }) => {
-  console.log('rendering ListScreen: ', route.params);
-
+const ListScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 30 }}>List Screen</Text>
-      <Button title="push" onPress={() => navigation.push('List')} />
-      <Button
-        title="navigate"
-        onPress={() => navigation.navigate('List', { ts: Date.now() })}
-      />
     </View>
   );
 };
