2c1a330428575df7db75e6b2400c56122f8433cf
Beomjun <alchemist85k@gmail.com>
2022-08-01 23:36:59 -0700

    3.1.3

--- rn-calc/src/App.js
+++ rn-calc/src/App.js
@@ -27,6 +27,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
