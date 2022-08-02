a2556d760a6b1c836588866070ae362f5ed4f947
Beomjun <alchemist85k@gmail.com>
2022-08-02 01:37:13 -0700

    6.1.5.2

---
+++ rn-todo/src/components/List.js
@@ -0,0 +1,37 @@
+import { FlatList, StyleSheet, View } from 'react-native';
+import { GRAY } from '../colors';
+import PropTypes from 'prop-types';
+import ListItem from './ListItem';
+
+const Separator = () => {
+  return <View style={styles.separator}></View>;
+};
+
+const List = ({ data }) => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => <ListItem item={item} />}
+      windowSize={2}
+      ItemSeparatorComponent={Separator}
+      ListHeaderComponent={View}
+      ListHeaderComponentStyle={{ height: 10 }}
+    />
+  );
+};
+
+List.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 1,
+    backgroundColor: GRAY.LIGHT,
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+});
+
+export default List;
--- rn-todo/src/screens/ListScreen.js
+++ rn-todo/src/screens/ListScreen.js
@@ -1,41 +1,17 @@
-import { FlatList, StyleSheet, View } from 'react-native';
-import ListItem from '../components/ListItem';
-import { GRAY } from '../colors';
+import { View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import EmptyList from '../components/EmptyList';
-
-const Separator = () => {
-  return <View style={styles.separator}></View>;
-};
+import List from '../components/List';
 
 const ListScreen = () => {
+  const { bottom } = useSafeAreaInsets();
   const todos = [];
 
-  return todos.length ? (
-    <View style={styles.container}>
-      <FlatList
-        data={todos}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <ListItem item={item} />}
-        windowSize={5}
-        ItemSeparatorComponent={Separator}
-        ListHeaderComponent={() => <View style={{ height: 10 }}></View>}
-      />
+  return (
+    <View style={{ flex: 1, paddingBottom: bottom }}>
+      {todos.length ? <List data={todos} /> : <EmptyList />}
     </View>
-  ) : (
-    <EmptyList />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  separator: {
-    height: 1,
-    backgroundColor: GRAY.LIGHT,
-    marginVertical: 10,
-    marginHorizontal: 10,
-  },
-});
 
 export default ListScreen;
