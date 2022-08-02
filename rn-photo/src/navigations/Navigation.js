74d3899630cf2497a4905703549cf6e8c1da309d
Beomjun <alchemist85k@gmail.com>
2022-08-02 14:58:24 -0700

    7.5.1

--- rn-photo/src/navigations/Navigation.js
+++ rn-photo/src/navigations/Navigation.js
@@ -1,9 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
 
 const Navigation = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onReady}>
       <AuthStack />
     </NavigationContainer>
   );
