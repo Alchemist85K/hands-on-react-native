6b233b4e04ead7e0884ad2081cba4927b8d6c3d8
Beomjun <alchemist85k@gmail.com>
2022-08-02 01:05:08 -0700

    5.3

--- rn-todo/src/screens/SignInScreen.js
+++ rn-todo/src/screens/SignInScreen.js
@@ -83,8 +83,8 @@ SignInScreen.propTypes = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   image: {
     width: 200,
