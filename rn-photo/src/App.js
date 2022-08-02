a1cdd057eedc891215bbddf98d49cadcf5baed09
Beomjun <alchemist85k@gmail.com>
2022-08-02 15:07:40 -0700

    8.1.2.3

--- rn-photo/src/App.js
+++ rn-photo/src/App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
+import { LogBox } from 'react-native';
 import Navigation from './navigations/Navigation';
 
 const App = () => {
+  LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
+
   return (
     <>
       <StatusBar style="dark" />
