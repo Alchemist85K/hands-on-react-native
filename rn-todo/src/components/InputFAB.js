1b2611d47678dd24f3e79d012398edaf1b9bb306
Beomjun <alchemist85k@gmail.com>
2022-08-02 01:42:30 -0700

    6.2.1.2

--- rn-todo/src/components/InputFAB.js
+++ rn-todo/src/components/InputFAB.js
@@ -1,31 +1,96 @@
-import { Pressable, StyleSheet } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  TextInput,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 import { PRIMARY, WHITE } from '../colors';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useRef, useState } from 'react';
 
 const InputFAB = () => {
+  const [text, setText] = useState('');
+  const [isOpened, setIsOpened] = useState(false);
+  const inputRef = useRef();
+  const windowWidth = useWindowDimensions().width;
+
+  const open = () => {
+    inputRef.current.focus();
+    setIsOpened(true);
+  };
+
+  const close = () => {
+    if (isOpened) {
+      inputRef.current.blur();
+      setText('');
+      setIsOpened(false);
+    }
+  };
+
+  const onPressButton = () => {
+    isOpened ? close() : open();
+  };
+
   return (
-    <Pressable
-      style={({ pressed }) => [
-        styles.button,
-        pressed && { backgroundColor: PRIMARY.DARK },
-      ]}
-    >
-      <MaterialCommunityIcons name="plus" size={24} color={WHITE} />
-    </Pressable>
+    <>
+      <View
+        style={[
+          styles.position,
+          styles.shape,
+          { justifyContent: 'center' },
+          isOpened && { width: windowWidth - 20 },
+        ]}
+      >
+        <TextInput
+          ref={inputRef}
+          onBlur={close}
+          value={text}
+          onChangeText={(text) => setText(text)}
+          style={[styles.input]}
+          autoCapitalize="none"
+          autoCorrect={false}
+          textContentType="none"
+          keyboardAppearance="light"
+          returnKeyType="done"
+        />
+      </View>
+
+      <Pressable
+        style={({ pressed }) => [
+          styles.position,
+          styles.shape,
+          styles.button,
+          pressed && { backgroundColor: PRIMARY.DARK },
+        ]}
+        onPress={onPressButton}
+      >
+        <MaterialCommunityIcons name="plus" size={24} color={WHITE} />
+      </Pressable>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
+  position: {
     position: 'absolute',
     bottom: 30,
     right: 10,
-    width: 60,
+  },
+  shape: {
     height: 60,
+    width: 60,
     borderRadius: 30,
+    backgroundColor: PRIMARY.DEFAULT,
+  },
+  input: {
+    color: WHITE,
+    paddingLeft: 20,
+    paddingRight: 70,
+  },
+  button: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: PRIMARY.DEFAULT,
   },
 });
 
