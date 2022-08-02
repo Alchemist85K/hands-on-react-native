245bc7ffd1c646b9861d656227d8415c4eebf47e
Beomjun <alchemist85k@gmail.com>
2022-08-02 01:59:57 -0700

    6.3

--- rn-todo/src/screens/ListScreen.js
+++ rn-todo/src/screens/ListScreen.js
@@ -1,19 +1,47 @@
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import EmptyList from '../components/EmptyList';
 import List from '../components/List';
 import InputFAB from '../components/InputFAB';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 const ListScreen = () => {
   const { bottom } = useSafeAreaInsets();
   const [todos, setTodos] = useState([]);
   const [isBottom, setIsBottom] = useState(false);
 
+  const { getItem, setItem } = useAsyncStorage('todos');
+
+  const save = async (data) => {
+    try {
+      await setItem(JSON.stringify(data));
+      setTodos(data);
+    } catch (e) {
+      Alert.alert('저장하기 실패', '데이터 저장에 실패했습니다.');
+    }
+  };
+
+  const load = async () => {
+    try {
+      const data = await getItem();
+      const todos = JSON.parse(data || '[]');
+      setTodos(todos);
+    } catch (e) {
+      Alert.alert('불러오기 실패', '데이터 불러오기에 실패했습니다.');
+    }
+  };
+
+  useEffect(() => {
+    load();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const onInsert = (task) => {
     const id = nanoid();
-    setTodos((prev) => [{ id, task, isDone: false }, ...prev]);
+    const newTodos = [{ id, task, isDone: false }, ...todos];
+    save(newTodos);
   };
 
   return (
