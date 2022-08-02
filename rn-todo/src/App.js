728c08c93b3fcc422e7e70fe168bc03609f14fef
Beomjun <alchemist85k@gmail.com>
2022-08-02 00:42:43 -0700

    5.1.1.2

--- rn-todo/src/App.js
+++ rn-todo/src/App.js
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 import SignInScreen from './screens/SignInScreen';
-import { WHITE } from './colors';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <StatusBar style="dark" />
       <SignInScreen />
-    </View>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: WHITE,
-  },
-});
 
 export default App;
