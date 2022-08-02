707086b61a9ec351e5bf3a9b3d175b82bbd3b4c9
Beomjun <alchemist85k@gmail.com>
2022-08-02 15:39:53 -0700

    8.4.4.2

--- rn-photo/src/components/FastImage.js
+++ rn-photo/src/components/FastImage.js
@@ -1,9 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Image } from 'react-native';
 import PropTypes from 'prop-types';
+import * as FileSystem from 'expo-file-system';
+import * as Crypto from 'expo-crypto';
 
 const FastImage = ({ source, ...props }) => {
   const [uri, setUri] = useState(source.uri);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const hashed = await Crypto.digestStringAsync(
+          Crypto.CryptoDigestAlgorithm.SHA256,
+          source.uri
+        );
+        const fileSystemUri = `${FileSystem.cacheDirectory}${hashed}`;
+
+        const metadata = await FileSystem.getInfoAsync(fileSystemUri);
+        if (!metadata.exists) {
+          await FileSystem.downloadAsync(source.uri, fileSystemUri);
+        }
+        setUri(fileSystemUri);
+      } catch (e) {
+        setUri(source.uri);
+      }
+    })();
+  }, [source.uri]);
 
   return <Image source={{ uri }} {...props} />;
 };
