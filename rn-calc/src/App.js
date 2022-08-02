3b6e804afbe4b6f084e2bcb75fa637db30709a90
Beomjun <alchemist85k@gmail.com>
2022-08-01 23:15:46 -0700

    2.1

---
+++ rn-calc/src/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Calc App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
