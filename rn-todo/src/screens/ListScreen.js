bce6a76b7b5c4a261c456b4bf7bd6e2487d67fec
Beomjun <alchemist85k@gmail.com>
2022-08-02 01:28:11 -0700

    6.1.2.1

--- rn-todo/src/screens/ListScreen.js
+++ rn-todo/src/screens/ListScreen.js
@@ -3,18 +3,19 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 const ListScreen = () => {
   const todos = [];
   for (let i = 1; i < 501; i++) {
-    todos.push({ value: i });
+    todos.push({ id: i, task: `task :: ${i}` });
   }
 
   return (
     <View style={styles.container}>
       <FlatList
         data={todos}
+        keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => {
-          console.log(item.value);
+          console.log(item.id);
           return (
             <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
-              <Text style={{ fontSize: 20 }}>{item.value}</Text>
+              <Text style={{ fontSize: 20 }}>{item.task}</Text>
             </View>
           );
         }}
