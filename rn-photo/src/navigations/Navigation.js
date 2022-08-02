12a282218e1f4f7e89d24459b6a4840ca5a253dd
Beomjun <alchemist85k@gmail.com>
2022-08-02 14:59:05 -0700

    7.5.2

--- rn-photo/src/navigations/Navigation.js
+++ rn-photo/src/navigations/Navigation.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
+import { Asset } from 'expo-asset';
 
 const Navigation = () => {
   const [isReady, setIsReady] = useState(false);
@@ -10,7 +11,11 @@ const Navigation = () => {
     (async () => {
       try {
         await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(
+          require('../../assets/cover.png')
+        ).downloadAsync();
       } catch (e) {
+        // eslint-disable-next-line no-console
         console.log(e);
       } finally {
         setIsReady(true);
