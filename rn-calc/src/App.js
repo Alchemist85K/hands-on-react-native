96304a65b27c3f491a74b902d12886413c362793
Beomjun <alchemist85k@gmail.com>
2022-08-01 23:20:37 -0700

    2.3.1

--- rn-calc/src/App.js
+++ rn-calc/src/App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
-  const isError = true;
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text style={styles.text}>Calc App</Text>
-      <Text style={[styles.error, styles.text]}>StyleSheet</Text>
-      <Text style={[styles.text, isError && styles.error]}>Error</Text>
+      <Button title="button" onPress={() => console.log('click!')} />
     </View>
   );
 };
