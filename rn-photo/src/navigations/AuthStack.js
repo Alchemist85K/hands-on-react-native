c88bef865e7432435f272e1605d705b7782057dd
Beomjun <alchemist85k@gmail.com>
2022-08-02 14:30:39 -0700

    7.4.1.1

--- rn-photo/src/navigations/AuthStack.js
+++ rn-photo/src/navigations/AuthStack.js
@@ -9,7 +9,10 @@ const Stack = createNativeStackNavigator();
 const AuthStack = () => {
   return (
     <Stack.Navigator
-      screenOptions={{ contentStyle: { backgroundColor: WHITE } }}
+      screenOptions={{
+        contentStyle: { backgroundColor: WHITE },
+        headerShown: false,
+      }}
     >
       <Stack.Screen name={AuthRoutes.SIGN_IN} component={SignInScreen} />
       <Stack.Screen name={AuthRoutes.SIGN_UP} component={SignUpScreen} />
--- rn-photo/src/screens/SignInScreen.js
+++ rn-photo/src/screens/SignInScreen.js
@@ -4,44 +4,49 @@ import { AuthRoutes } from '../navigations/routes';
 import Input, { ReturnKeyTypes, InputTypes } from '../components/Input';
 import { useState } from 'react';
 import Button from '../components/Button';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import SafeInputView from '../components/SafeInputView';
 
 const SignInScreen = () => {
   const navigation = useNavigation();
+  const { top } = useSafeAreaInsets();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   return (
-    <View style={styles.container}>
-      <Text>Sign In</Text>
-      <Input
-        styles={{
-          container: { marginBottom: 20, paddingHorizontal: 20 },
-          input: { borderWidth: 1 },
-        }}
-        value={email}
-        onChangeText={(text) => setEmail(text.trim())}
-        inputType={InputTypes.EMAIL}
-        returnKeyType={ReturnKeyTypes.NEXT}
-      />
-      <Input
-        styles={inputStyles}
-        value={password}
-        onChangeText={(text) => setPassword(text.trim())}
-        inputType={InputTypes.PASSWORD}
-        returnKeyType={ReturnKeyTypes.DONE}
-      />
-      <Button
-        title="Sign Up"
-        onPress={() => navigation.navigate(AuthRoutes.SIGN_UP)}
-        styles={{
-          container: {
-            paddingHorizontal: 20,
-            marginTop: 20,
-          },
-        }}
-      />
-    </View>
+    <SafeInputView>
+      <View style={[styles.container, { paddingTop: top }]}>
+        <Text>Sign In</Text>
+        <Input
+          styles={{
+            container: { marginBottom: 20, paddingHorizontal: 20 },
+            input: { borderWidth: 1 },
+          }}
+          value={email}
+          onChangeText={(text) => setEmail(text.trim())}
+          inputType={InputTypes.EMAIL}
+          returnKeyType={ReturnKeyTypes.NEXT}
+        />
+        <Input
+          styles={inputStyles}
+          value={password}
+          onChangeText={(text) => setPassword(text.trim())}
+          inputType={InputTypes.PASSWORD}
+          returnKeyType={ReturnKeyTypes.DONE}
+        />
+        <Button
+          title="Sign Up"
+          onPress={() => navigation.navigate(AuthRoutes.SIGN_UP)}
+          styles={{
+            container: {
+              paddingHorizontal: 20,
+              marginTop: 20,
+            },
+          }}
+        />
+      </View>
+    </SafeInputView>
   );
 };
 
