da727880cf75fe9f2f8a8b30ce290d452ba1a878
Beomjun <alchemist85k@gmail.com>
2022-08-02 00:22:21 -0700

    4.5.4

--- rn-todo/src/screens/SignInScreen.js
+++ rn-todo/src/screens/SignInScreen.js
@@ -1,41 +1,26 @@
-import {
-  Image,
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  Pressable,
-  StyleSheet,
-  View,
-} from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
+import SafeInputView from '../components/SafeInputView';
 
 const SignInScreen = () => {
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.select({ ios: 'padding' })}
-    >
-      <Pressable style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
-        <View style={styles.container}>
-          <Image
-            source={require('../../assets/main.png')}
-            style={styles.image}
-          />
+    <SafeInputView>
+      <View style={styles.container}>
+        <Image source={require('../../assets/main.png')} style={styles.image} />
 
-          <Input
-            title={'이메일'}
-            placeholder="your@email.com"
-            keyboardType={KeyboardTypes.EMAIL}
-            returnKeyType={ReturnKeyTypes.NEXT}
-          />
-          <Input
-            title={'비밀번호'}
-            returnKeyType={ReturnKeyTypes.DONE}
-            secureTextEntry
-          />
-        </View>
-      </Pressable>
-    </KeyboardAvoidingView>
+        <Input
+          title={'이메일'}
+          placeholder="your@email.com"
+          keyboardType={KeyboardTypes.EMAIL}
+          returnKeyType={ReturnKeyTypes.NEXT}
+        />
+        <Input
+          title={'비밀번호'}
+          returnKeyType={ReturnKeyTypes.DONE}
+          secureTextEntry
+        />
+      </View>
+    </SafeInputView>
   );
 };
 
