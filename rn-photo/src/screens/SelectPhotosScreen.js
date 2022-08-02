e0bebcfb21ea0dc34ee24a7eef7ac8c72244086e
Beomjun <alchemist85k@gmail.com>
2022-08-02 16:51:53 -0700

    10.1.1.1

--- rn-photo/src/screens/SelectPhotosScreen.js
+++ rn-photo/src/screens/SelectPhotosScreen.js
@@ -1,17 +1,38 @@
 import { useNavigation } from '@react-navigation/native';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+  Pressable,
+} from 'react-native';
 import { MainRoutes } from '../navigations/routes';
+import { GRAY, WHITE } from '../colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const SelectPhotosScreen = () => {
   const navigation = useNavigation();
 
+  const width = useWindowDimensions().width;
+
   return (
     <View style={styles.container}>
-      <Text>Select Photos</Text>
-      <Button
-        title="Tab"
-        onPress={() => navigation.navigate(MainRoutes.CONTENT_TAB)}
-      />
+      <Text style={styles.description}>
+        이미지는 최대 4장까지 선택 가능합니다.
+      </Text>
+
+      <View style={{ width, height: width }}>
+        <Pressable
+          style={styles.photoButton}
+          onPress={() => navigation.navigate(MainRoutes.IMAGE_PICKER)}
+        >
+          <MaterialCommunityIcons
+            name="image-plus"
+            size={80}
+            color={GRAY.DEFAULT}
+          />
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -19,8 +40,19 @@ const SelectPhotosScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: WHITE,
+  },
+  description: {
+    color: GRAY.DARK,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  photoButton: {
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: GRAY.LIGHT,
   },
 });
 
