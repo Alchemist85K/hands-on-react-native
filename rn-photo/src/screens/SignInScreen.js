238b44f89820c5d5f6790ebf5a0329c3dc6ced10
Beomjun <alchemist85k@gmail.com>
2022-08-02 14:32:44 -0700

    7.4.1.2

--- rn-photo/src/screens/SignInScreen.js
+++ rn-photo/src/screens/SignInScreen.js
@@ -1,8 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Keyboard } from 'react-native';
 import { AuthRoutes } from '../navigations/routes';
 import Input, { ReturnKeyTypes, InputTypes } from '../components/Input';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Button from '../components/Button';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import SafeInputView from '../components/SafeInputView';
@@ -11,60 +11,65 @@ const SignInScreen = () => {
   const navigation = useNavigation();
   const { top } = useSafeAreaInsets();
 
+  const passwordRef = useRef();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    setDisabled(!email || !password);
+  }, [email, password]);
+
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    if (!disabled && !isLoading) {
+      setIsLoading(true);
+      console.log(email, password);
+      setIsLoading(false);
+    }
+  };
 
   return (
     <SafeInputView>
       <View style={[styles.container, { paddingTop: top }]}>
-        <Text>Sign In</Text>
         <Input
-          styles={{
-            container: { marginBottom: 20, paddingHorizontal: 20 },
-            input: { borderWidth: 1 },
-          }}
           value={email}
           onChangeText={(text) => setEmail(text.trim())}
           inputType={InputTypes.EMAIL}
           returnKeyType={ReturnKeyTypes.NEXT}
+          onSubmitEditing={() => passwordRef.current.focus()}
+          styles={{ container: { marginBottom: 20 } }}
         />
         <Input
-          styles={inputStyles}
+          ref={passwordRef}
           value={password}
           onChangeText={(text) => setPassword(text.trim())}
           inputType={InputTypes.PASSWORD}
           returnKeyType={ReturnKeyTypes.DONE}
+          onSubmitEditing={onSubmit}
+          styles={{ container: { marginBottom: 20 } }}
         />
+
         <Button
-          title="Sign Up"
-          onPress={() => navigation.navigate(AuthRoutes.SIGN_UP)}
-          styles={{
-            container: {
-              paddingHorizontal: 20,
-              marginTop: 20,
-            },
-          }}
+          title="로그인"
+          onPress={onSubmit}
+          disabled={disabled}
+          isLoading={isLoading}
+          styles={{ container: { marginTop: 20 } }}
         />
       </View>
     </SafeInputView>
   );
 };
 
-const inputStyles = StyleSheet.create({
-  container: {
-    marginBottom: 20,
-    paddingHorizontal: 20,
-  },
-  input: {
-    borderWidth: 1,
-  },
-});
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 20,
   },
 });
 
