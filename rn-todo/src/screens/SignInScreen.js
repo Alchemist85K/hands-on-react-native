08cc3e6a1c725518b6f593b41c5e9ad20dec417f
Beomjun <alchemist85k@gmail.com>
2022-08-02 00:21:25 -0700

    4.5.3

--- rn-todo/src/screens/SignInScreen.js
+++ rn-todo/src/screens/SignInScreen.js
@@ -1,7 +1,9 @@
 import {
   Image,
+  Keyboard,
   KeyboardAvoidingView,
   Platform,
+  Pressable,
   StyleSheet,
   View,
 } from 'react-native';
@@ -13,21 +15,26 @@ const SignInScreen = () => {
       style={{ flex: 1 }}
       behavior={Platform.select({ ios: 'padding' })}
     >
-      <View style={styles.container}>
-        <Image source={require('../../assets/main.png')} style={styles.image} />
+      <Pressable style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
+        <View style={styles.container}>
+          <Image
+            source={require('../../assets/main.png')}
+            style={styles.image}
+          />
 
-        <Input
-          title={'이메일'}
-          placeholder="your@email.com"
-          keyboardType={KeyboardTypes.EMAIL}
-          returnKeyType={ReturnKeyTypes.NEXT}
-        />
-        <Input
-          title={'비밀번호'}
-          returnKeyType={ReturnKeyTypes.DONE}
-          secureTextEntry
-        />
-      </View>
+          <Input
+            title={'이메일'}
+            placeholder="your@email.com"
+            keyboardType={KeyboardTypes.EMAIL}
+            returnKeyType={ReturnKeyTypes.NEXT}
+          />
+          <Input
+            title={'비밀번호'}
+            returnKeyType={ReturnKeyTypes.DONE}
+            secureTextEntry
+          />
+        </View>
+      </Pressable>
     </KeyboardAvoidingView>
   );
 };
