c44c5a8a37cc0800fc63bdba51298b6e674a25c9
Beomjun <alchemist85k@gmail.com>
2022-08-02 00:26:00 -0700

    4.7.2

--- rn-todo/src/components/Input.js
+++ rn-todo/src/components/Input.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import PropTypes from 'prop-types';
-import { GRAY } from '../colors';
+import { GRAY, PRIMARY } from '../colors';
+import { useState } from 'react';
 
 export const KeyboardTypes = {
   DEFAULT: 'default',
@@ -13,18 +14,24 @@ export const ReturnKeyTypes = {
 };
 
 const Input = ({ title, placeholder, ...props }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
+      <Text style={[styles.title, isFocused && styles.focusedTitle]}>
+        {title}
+      </Text>
       <TextInput
         {...props}
-        style={styles.input}
+        style={[styles.input, isFocused && styles.focusedInput]}
         placeholder={placeholder ?? title}
         placeholderTextColor={GRAY.DEFAULT}
         autoCapitalize="none"
         autoCorrect={false}
         textContentType="none"
         keyboardAppearance="light"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
     </View>
   );
@@ -43,12 +50,23 @@ const styles = StyleSheet.create({
   },
   title: {
     marginBottom: 4,
+    color: GRAY.DEFAULT,
+  },
+  focusedTitle: {
+    fontWeight: '600',
+    color: PRIMARY.DEFAULT,
   },
   input: {
     borderWidth: 1,
     borderRadius: 8,
     paddingHorizontal: 10,
     height: 42,
+    borderColor: GRAY.DEFAULT,
+  },
+  focusedInput: {
+    borderWidth: 2,
+    borderColor: PRIMARY.DEFAULT,
+    color: PRIMARY.DEFAULT,
   },
 });
 
