3ef22ec65031bad0b2b743d71f316cf57d65dd38
Beomjun <alchemist85k@gmail.com>
2022-08-02 01:33:15 -0700

    6.1.4.4

--- rn-todo/src/components/ListItem.js
+++ rn-todo/src/components/ListItem.js
@@ -1,11 +1,23 @@
 import { memo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { DANGER } from '../colors';
 
 const ListItem = memo(({ item }) => {
   return (
     <View style={styles.container}>
-      <Text>{item.task}</Text>
+      <View style={styles.task}>
+        <Text>{item.task}</Text>
+      </View>
+
+      <Pressable onPress={() => {}} hitSlop={10}>
+        <MaterialCommunityIcons
+          name="trash-can"
+          size={20}
+          color={DANGER.DEFAULT}
+        />
+      </Pressable>
     </View>
   );
 });
@@ -22,6 +34,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderWidth: 1,
     borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  task: {
+    flex: 1,
+    marginHorizontal: 10,
   },
 });
 
