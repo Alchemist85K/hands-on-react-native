0e7cdda4869232be6f0ca69c4cb9715802eddc82
Beomjun <alchemist85k@gmail.com>
2022-08-02 17:52:45 -0700

    10.6.3.6

--- rn-photo/src/components/LocationPostList.js
+++ rn-photo/src/components/LocationPostList.js
@@ -2,6 +2,19 @@ import { FlatList, View } from 'react-native';
 import usePostsByLocation from '../hooks/usePostsByLocation';
 import FastImage from './FastImage';
 import PropTypes from 'prop-types';
+import { memo } from 'react';
+
+const LocationPostItem = memo(({ uri }) => {
+  return (
+    <View style={{ paddingHorizontal: 5 }}>
+      <FastImage source={{ uri }} style={{ width: 150, height: 150 }} />
+    </View>
+  );
+});
+LocationPostItem.displayName = 'LocationPostItem';
+LocationPostItem.propTypes = {
+  uri: PropTypes.string.isRequired,
+};
 
 const LocationPostList = ({ location }) => {
   const { data, fetchNextPage } = usePostsByLocation(location);
@@ -11,14 +24,7 @@ const LocationPostList = ({ location }) => {
       horizontal={true}
       data={data}
       keyExtractor={(_, idx) => idx.toString()}
-      renderItem={({ item }) => (
-        <View style={{ paddingHorizontal: 5 }}>
-          <FastImage
-            source={{ uri: item }}
-            style={{ width: 150, height: 150 }}
-          />
-        </View>
-      )}
+      renderItem={({ item }) => <LocationPostItem uri={item} />}
       onEndReached={fetchNextPage}
     />
   );
