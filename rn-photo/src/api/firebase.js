4a3c849bcbae3f590c78e05aaed9ffdf814a6273
Beomjun Kim <alchemist85k@gmail.com>
2023-03-21 02:17:29 -0700

    upgrade to Expo 48 and resolve onAuthStateChanged issue

--- rn-photo/src/api/firebase.js
+++ rn-photo/src/api/firebase.js
@@ -1,6 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '../../env';
+import {
+  getReactNativePersistence,
+  initializeAuth,
+} from 'firebase/auth/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const initFirebase = () => {
-  return initializeApp(firebaseConfig);
+  try {
+    const app = initializeApp(firebaseConfig);
+    initializeAuth(app, {
+      persistence: getReactNativePersistence(AsyncStorage),
+    });
+    return app;
+  } catch (e) {
+    // console.log(e);
+  }
 };
